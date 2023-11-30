f6ed61c18773dd5561cfd364030291b5d7c0db10
Andrew Huth <git@huth.me>
2023-11-30 08:12:34 -0500

    Import the .wasm file directly

--- app/renderer.tsx
+++ app/renderer.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 
 export default dynamic(
   async () => {
-    const {add} = await import('@/build/release');
+    const {add} = await import('@/build/release.wasm');
 
     return function Renderer() {
       return <span>{add(1, 2)}</span>
