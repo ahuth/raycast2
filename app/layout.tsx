82ba53741d99d853a6bff67508eb20b9d35e60d1
Andrew Huth <git@huth.me>
2023-12-03 22:18:09 -0500

    Remove unneeded styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Raycast 2',
@@ -17,7 +14,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
