323701ca1969152384a94bf46f20f502d87be12a
Andrew Huth <git@huth.me>
2023-12-07 10:45:14 -0500

    Externalize asc's generated 'fs' dep

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Used by the output of asc for Node targets. Won't run in the browser, so we can just
+        // mark it as external.
+        'node:fs/promises',
+      ],
+    },
+  },
 })
