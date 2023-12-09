59d9841abe739bb4bd60338ddb42dd69c4fe27f3
Andrew Huth <git@huth.me>
2023-12-09 11:13:35 -0500

    Capture keys and cast/draw on every frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
 import {useEffect, useRef} from 'react';
-import {memory, cast, NUM_RAYS} from '../build/release';
+import {memory, cast, update, NUM_RAYS} from '../build/release';
 import draw from './draw';
 
 export default function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const keysRef = useRef<Record<string, boolean>>({});
 
+  // Capture key ups and key downs.
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      keysRef.current[event.key] = true;
+    }
+
+    function handleKeyUp(event: KeyboardEvent) {
+      keysRef.current[event.key] = false;
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  // Cast rays and draw to the canvas.
   useEffect(() => {
     if (canvasRef.current) {
       const context = canvasRef.current.getContext('2d')!;
       const data = new DataView(memory.buffer);
       cast();
       draw(context, data, NUM_RAYS.value);
+
+      let id = -1;
+
+      // eslint-disable-next-line no-inner-declarations
+      function raycast() {
+        const {ArrowUp, ArrowDown, ArrowLeft, ArrowRight} = keysRef.current;
+        update(ArrowUp, ArrowDown, ArrowLeft, ArrowRight);
+        cast();
+        draw(context, data, NUM_RAYS.value);
+        id = window.requestAnimationFrame(raycast);
+      }
+
+      id =  window.requestAnimationFrame(raycast);
+
+      return () => window.cancelAnimationFrame(id);
     }
   }, []);
 
